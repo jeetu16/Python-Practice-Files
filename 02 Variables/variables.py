f6732ba8342033d904f66c1bf9f8572fc5272d74
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-17 11:30:07 +0530

    First Commit

---
+++ 02 Variables/variables.py
@@ -0,0 +1,54 @@
+# ---> variable = variable is a container which hold the data and create memory allocation for the date. We can also say that variable is a label for the data like yourName, age, mobileNumber, address ect.
+
+full_name = "Jeetu Dewangan"
+
+# 3 different way to output a variable along with the syntax
+
+# print("My name is ",full_name)
+# print("My name is "+full_name)
+# print(f'My name is {full_name}')
+
+# ---> Basic Date Types
+
+# ---> Number 
+
+age = 25
+player = 11
+quantify = 4
+
+# print(f'You are {age} years old')
+# print(f"Ther are {player} players online")
+# print(f"You would like to buy {quantify} items")
+
+
+# ---> Float
+
+gpa = 7.2
+distance = 2.5
+price = 10.99
+
+# print(f'Your gpa is {gpa}')
+# print(f'You ran {distance}km')
+# print(f"The price is ${price}")
+
+
+# ---> String
+
+name = "Jeetu"
+food = "Pizza"
+email = "abc@gmail.com"
+
+# print(f"Hello, {name}")
+# print(f"You like {food}")
+# print(f"My email address is {email}")
+
+
+# ---> Boolean
+
+online = True
+for_sale = False
+running = False
+
+print(f'Are you Online?: {online}')
+print(f"Is this item for sale?: {for_sale}")
+print(f"Game running: {running}")
