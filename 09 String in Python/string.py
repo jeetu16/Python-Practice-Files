ee717a2d562f238118f13153a3cad94e8428ffe8
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-22 18:27:51 +0530

    minor updates

--- 09 String in Python/string.py
+++ 09 String in Python/string.py
@@ -11,22 +11,24 @@
 '''
 
 # ---> String Indexing
-'''
+
 # indexing = accessing elements of a sequence using [] (indexing operator)
 #                     [start : end : step]
-
+'''
 credit_number = "1234-5678-9012-3456"
 
-print(credit_number[0])
-print(credit_number[0:4])
-print(credit_number[:4])
-print(credit_number[4:])
-print(credit_number[-1])
-print(credit_number[-4:])
-print(credit_number[::2])
-print(credit_number[::3])
+print(credit_number[0])     # '1'
+print(credit_number[0:4])   # '1234'
+print(credit_number[:4])    # '1234'
+print(credit_number[4:])    # '-5678-9012-3456'
+print(credit_number[-1])    # '6'
+print(credit_number[-4:])   # '3456'
+print(credit_number[::2])   # '13-6891-46'
+print(credit_number[::3])   # '146-136'
 '''
 
+
+"""
 # ---> EXERCISE 1
 
 credit_number = "1234-5678-9012-3456"
@@ -38,3 +40,4 @@ print(f"XXXX-XXXX-XXXX-{last_digits}")
 credit_number = "1234-5678-9012-3456"
 credit_number = credit_number[::-1]
 print(credit_number)
+"""
