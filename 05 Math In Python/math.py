53e20b146d76dc5b84b5050b1eded3480074b763
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-22 18:12:26 +0530

    minor updates

--- 05 Math In Python/math.py
+++ 05 Math In Python/math.py
@@ -18,7 +18,7 @@ friend = 10
 # friend /= 2           # Augmented Assignment Operator
 
 # friend = friend**2    # exponent Operator
-# friend **= 2            # Augmented Assignment Operator
+# friend **= 2          # Augmented Assignment Operator
 
 # friend = friend % 3   # Modulus(%) operator
 # friend %= 3           # Augmented Assignment Operator
@@ -34,25 +34,30 @@ y = 40
 z = -20
 
 # result = round(3)           # round(value,optional)
-# result = abs(z)
-# result = pow(4,3)
-# result = max(x,y,z)
-# result = min(x,y,z)
+# result = abs(z)             # 20
+# result = pow(4,3)           # 64
+# result = max(x,y,z)         # 40
+# result = min(x,y,z)         # -20
 print(f"Result is: {result}")
 """
 
+
 # ---> Contant Values in Python
-"""
-# constant = math.pi
-# constant = math.e
+'''
+constant = math.pi          # 3.141592653589793
+constant = math.e           # 2.718281828459045
 print(f"Contant value:{constant}")
-"""
-"""
-answer = math.sqrt(5)
-answer = math.ceil(9.12)
-answer = math.floor(9.95)
+'''
+
+
+# ---> other math function in python 
+'''
+answer = math.sqrt(5)       # 2.23606797749979
+answer = math.ceil(9.12)    # 10
+answer = math.floor(9.95)   # 9
 print(f"Answer :{answer}")
-"""
+'''
+
 
 # ---> Exercise 1 : Circumference of circle
 """"
@@ -61,12 +66,14 @@ circumference = 2 * math.pi * radius
 print(f"The circumference of Circle is: {round(circumference,2)}cm")
 """
 
+
 # ---> Exercise 2 : Area of cicle
 '''
 radius = float(input("Enter the radius of a cirlce: "))
 area = math.pi * pow(radius,2)
 print(f"The area of the Circle is: {round(area,2)}cm^2")
 '''
+
 
 # ---> Exercise 3 : Hypotenuse of right angle triangle
 
