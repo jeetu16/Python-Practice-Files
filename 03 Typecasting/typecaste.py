3d5f41c5dc2353f4ac974d5abba8675f5416dd53
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-01-22 17:55:46 +0530

    minor changes

--- 03 Typecasting/typecaste.py
+++ 03 Typecasting/typecaste.py
@@ -19,28 +19,28 @@ age = 25
 gpa = 7.2
 student = True
 
-print(type(name))
-print(type(age))
-print(type(gpa))
-print(type(student))
+print(type(name))       # 'str'
+print(type(age))        # 'int'
+print(type(gpa))        # 'float'
+print(type(student))    # 'bool'
 
 # ---> int to float conversion
 age = float(age)
-print(age,type(age))
+print(age,type(age))    # 25 'float'
 
 # ---> float to int conversion
-gpa = int(gpa)
-print(gpa,type(gpa))
+gpa = int(gpa)      
+print(gpa,type(gpa))    # 7 'int'
 
 # ---> string to bool conversion
 name = bool(name);
-print(name,type(name))
+print(name,type(name))  # True 'bool'
 
 # ---> boolean to string conversion
 student = str(student)
-print(student,type(student))
-
+print(student,type(student))    # True 'str'
 '''
+
 
 # ---> int,float to boolean
 '''
@@ -48,8 +48,8 @@ num = 0.0
 num2 = -10   # except zero(0 or 0.0) in boolean everthing is true value considering only int and float here
 num = bool(num) 
 num2 = bool(num2)
-print(num, type(num))
-print(num2, type(num2))
+print(num, type(num))       # False 'bool'
+print(num2, type(num2))     # True 'bool'
 '''
 
 # ---> string to boolean
@@ -58,8 +58,8 @@ name = "Jeetu"
 name2 = ""
 name = bool(name)
 name2 = bool(name2) # except empty string ("",'') in boolean everything is True value considering only String here
-print(name,type(name))
-print(name2,type(name2))
+print(name,type(name))      # True 'bool'
+print(name2,type(name2))    # False 'False'
 '''
 
 # ---> int,float to string
@@ -68,8 +68,8 @@ number = 250
 float_num = 20.3
 number = str(number)
 float_num = str(float_num)
-print(number,type(number))
-print(float_num,type(float_num))
+print(number,type(number))          # 250 'str'
+print(float_num,type(float_num))    # 20.3 'str'
 '''
 
 # ---> string to int,float
@@ -84,10 +84,11 @@ name = int(name)
 int_str = int(int_str)
 float_str = float(float_str)
 
-print(name,type(name))
-print(int_str,type(int_str))
-print(float_str,type(float_str))
+print(name,type(name))              # ValueError
+print(int_str,type(int_str))        # 230 'int'
+print(float_str,type(float_str))    # 250.4 'float'
 '''
+
 
 # Falsy values in python are: 0, False, None, an empty string('',""), an empty list (e.g,[]), an empty tuple (e.g,()) and an emptye dictionary (e.g, {}).
 
