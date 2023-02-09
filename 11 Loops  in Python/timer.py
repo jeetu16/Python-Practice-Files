e334a6ed083a1ef388a21ac2b3c29c9599ba3773
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2023-02-09 22:30:38 +0530

    comment added

--- 11 Loops  in Python/timer.py
+++ 11 Loops  in Python/timer.py
@@ -8,7 +8,11 @@ for x in range(my_time, 0, -1):
     minutes = int(x / 60)
     hours = int(x/3600)
     print(f'{hours:02}:{minutes:02}:{seconds:02}')
-    time.sleep(1)
-    os.system('cls')    # clears the output screen
+    
+    #delay the execution of current thread in seconds(here 1 second)
+    time.sleep(2)
+
+    # clears the output screen
+    os.system('cls')
 
 print("Time's Up")
